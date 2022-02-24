9e2e7b7990be0282f2209dd78a93ee28ed2b4714
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-02-24 12:32:28 -0300

    movie tests passed

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks
+
+  before_destroy :ensure_no_bookmarks
+
+  validates :title, uniqueness: true, presence: true
+  validates :overview, presence: true
+
+  private
+
+  def ensure_no_bookmarks
+    self.errors[:base] << 'Cannot delete movie with bookmarks' unless self.bookmarks.empty?
+  end
 end
