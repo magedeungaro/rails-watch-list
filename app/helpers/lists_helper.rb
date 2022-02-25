a995b299fea345bac737964c3806439f93f31ccf
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-02-25 13:06:36 -0300

    refactored using cloudinary and active storage

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,5 +1,7 @@
 module ListsHelper
-  def prepend_image_gradient(url)
+  def prepend_image_gradient(url, options = {})
+    url = cl_image_path url if options[:cloudinary]
+
     color = 'rgba(0, 0, 0, 0.3)'
     "background-image: linear-gradient(#{color}, #{color}),url('#{url}')"
   end
